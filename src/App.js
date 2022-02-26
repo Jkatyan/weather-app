b885dc6c3184f2d1c00e130a8aafac935c78326d
Jay Katyan <33378744+Jkatyan@users.noreply.github.com>
2022-02-26 18:42:08 -0500

    Implemented radio buttons

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
   state = {
     zip_code: "",
     temperature: 0,
+    valid_response: false,
+    checked: "Fahrenheit",
     api_key: "a69a12410c5c4afe27ab1395d005f0be",
   };
 
@@ -22,13 +24,31 @@ class App extends Component {
 
   setZipCode(val) {
     this.setState({
-      zip_code: val.target.value
+      zip_code: val.target.value,
     });
   }
 
   setTemperature(val) {
     this.setState({
-      temperature: val
+      temperature: val,
+    });
+  }
+
+  setValidResponse(val) {
+    this.setState({
+      valid_response: val,
+    });
+  }
+
+  setFahrenheit(val) {
+    this.setState({
+      checked: "Fahrenheit",
+    });
+  }
+
+  setCelsius(val) {
+    this.setState({
+      checked: "Celsius",
     });
   }
 
@@ -37,23 +57,21 @@ class App extends Component {
       .get(this.createURL())
       .then((response) => {
         console.log(response);
+        this.setValidResponse(true);
         this.setTemperature(response.data.main.temp);
       })
       .catch((error) => {
+        this.setValidResponse(false);
         console.log(error);
       });
   }
 
   getFahrenheit(temp) {
-    return (
-      1.8 * (temp - 273.15) + 32
-    );
+    return 1.8 * (temp - 273.15) + 32;
   }
 
   getCelsius(temp) {
-    return (
-      temp - 273.15
-    );
+    return temp - 273.15;
   }
 
   render() {
@@ -61,6 +79,7 @@ class App extends Component {
       <div>
         <header className="App-header">
           <Form>
+            {/* Text Input */}
             <Form.Group className="mb-3">
               <Form.Label>What's the temperature?</Form.Label>
               <Form.Control
@@ -68,10 +87,27 @@ class App extends Component {
                 onChange={this.setZipCode.bind(this)}
               />
               <Form.Text className="text-muted">
-                Please enter a Postal / Zip Code.
+                Please enter a valid Zip Code.
               </Form.Text>
             </Form.Group>
 
+            {/* Select Type */}
+            <Form.Group>
+              <Form.Check
+                type={"radio"}
+                label={`Fahrenheit`}
+                checked={this.state.checked === "Fahrenheit"}
+                onChange={this.setFahrenheit.bind(this)}
+              />
+              <Form.Check
+                type={"radio"}
+                label={`Celsius`}
+                checked={this.state.checked === "Celsius"}
+                onChange={this.setCelsius.bind(this)}
+              />
+            </Form.Group>
+            <br />
+            {/* Submit Button */}
             <Button
               variant="primary"
               type="button"
@@ -80,7 +116,6 @@ class App extends Component {
               Get Temperature
             </Button>
           </Form>
-          {this.getCelsius(this.state.temperature)}
         </header>
       </div>
     );
