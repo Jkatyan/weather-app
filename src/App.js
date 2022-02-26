06fba0379af353e0e6a3e427072a7443f8d9c29f
Jay Katyan <33378744+Jkatyan@users.noreply.github.com>
2022-02-26 17:18:40 -0500

    Alerts functional

--- src/App.js
+++ src/App.js
@@ -1,17 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-//Components
-import UserInput from './components/UserInput';
+import axios from "axios";
+import { Form, Button, Alert } from "react-bootstrap";
 
-function App() {
-  return (
-    <div>
-      <header className="App-header">
-        <UserInput />
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    visible: false
+  };
+
+  toggle() {
+    this.setState({
+      visible: !this.state.visible
+    });
+  }
+
+  getZipCode(zip) {
+    /*
+    axios.get()
+    .then(response => {
+        console.log(reponse)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+    */
+  }
+
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>What's the temperature?</Form.Label>
+                <Form.Control
+                  type="entry"
+                  placeholder="Enter Zip Code"
+                  onChange={this.getZipCode}
+                />
+                <Form.Text className="text-muted">
+                  Please enter a Postal / Zip Code.
+                </Form.Text>
+              </Form.Group>
+
+              <Button variant="primary"
+                      type="button"
+                      onClick={this.toggle.bind(this)}>
+                Submit
+              </Button>
+            </Form>
+          </>
+          <br />
+          <Alert
+            variant="info"
+            show={this.state.visible}
+            onClose={this.toggle.bind(this)}
+            dismissible>
+            <Alert.Heading>Temperature:</Alert.Heading>
+            <p>
+              Current Temperature Here!
+            </p>
+          </Alert>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
