04ddd3c856493972d46e5b2f671771f8c70605a7
Jay Katyan <33378744+Jkatyan@users.noreply.github.com>
2022-02-27 14:04:28 -0500

    Optional query string

--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ const apiPort = 3000
 const apiKey = 'a69a12410c5c4afe27ab1395d005f0be'
 
 function getFahrenheit(temp) {
-    return 1.8 * (temp - 273.15) + 32;
+    return parseInt(1.8 * (temp - 273.15) + 32);
 }
 
 function getCelsius(temp) {
-    return temp - 273.15;
+    return parseInt(temp - 273.15);
 }
 
 app.get('/', (req, res) => {
@@ -27,20 +27,16 @@ app.get('/locations/:zipcode/:scale?', (req, res) => {
         function(error, response, body) {
             if(!error && response.statusCode == 200) {
                 var responseData = JSON.parse(body);
-                var temp = responseData.main.temp;
+                var temperature = responseData.main.temp;
                 var scale = 'Fahrenheit';
-                if (req.params.scale) {
-                    if (req.params.scale === 'celsius'){
-                        scale = 'Celsius';
-                        temp = getCelsius(temp);
-                        return res.send({temp, scale});
-                    }
-                    temp = getFahrenheit(temperature);
-                    return res.send({temp, scale});
+                if (req.query.scale === 'Celsius'){
+                    scale = 'Celsius';
+                    temperature = getCelsius(temperature);
+                    return res.send({temperature, scale});
                 }
                 else {
-                    temp = getFahrenheit(temp)
-                    return res.send({temp, scale})
+                temperature = getFahrenheit(temperature);
+                return res.send({temperature, scale});
                 }
             }
             else {
